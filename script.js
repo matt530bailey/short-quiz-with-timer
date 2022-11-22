e1c208d334754c6dd7830f45105fe31247577568
matt530bailey <115194527+matt530bailey@users.noreply.github.com>
2022-11-21 16:58:18 -0800

    adjusted timer location to not be overwritten as childEl, minor CSS adjustments, created alerts based on time, 'click' adjustments made to start_quiz_btn element

--- script.js
+++ script.js
@@ -1,41 +1,90 @@
 var start_quiz_btn = document.querySelector(".start_quiz_btn")
-console.log(start_quiz_btn)
-var timerEl = $('.timer')
-var timeRemaining = 30
-// timeRemaining = timeRemaining - 1
-// console.log(timeRemaining)
-// timeRemaining--
-// console.log(timeRemaining)
-// timeRemaining-=9
+var questionContainer = document.querySelector("#question_box")
+var timerEl = document.querySelector("#timer")
+var questionEl = document.querySelector(".question")
+var answerGrid = document.querySelector(".answers")
+var timeRemaining = 5
+var shuffledQuestions, currentQuestionIndex
+
 start_quiz_btn.addEventListener('click', () => {
     timer ();
-    // initiate first question
+    setNextQuestion();
+    start_quiz_btn.classList.add("hide");
+    questionContainer.classList.remove("hide");
+    timerEl.classList.remove('hide')
+    
+
 })
-
-function hideStart () {
-    firstChildBody.setAtribute = 'none';
-}
-// addEventListener('click', "start_quiz_btn.style.display = 'none'");
-
-function startGame () {
-    console.log("LINE 9")
-
-}
 
 function timer() {
     setInterval( () => {
         timeRemaining--
-        console.log(timeRemaining)
-    }, 1000 )
+        if (timeRemaining < 1) {
+            clearInterval(timerEl);
+            alert("Out of time!!");
+            return;
+
+        }
+        console.log(timeRemaining);
+    }, 1000 );
 }
 
 
+function setNextQuestion() {
+}
 
-// let timer = document.getElementsByClassName('timer');
+function showQuestion () {
+}
 
-// function startTimer () {
-//     EventTarget.addEventListener('click',)
-// }
+function selectAnswer () {
 
+}
 
-// console.log(document.getElementsByClassName('start_quiz_btn'));
+let questions = [
+    {
+        question: 'When does Benedictine claim to be invented?',
+        answers: [
+            { text: '1775', correct: false},
+            { text: '1738', correct: false},
+            { text: '1313', correct: false},
+            { text: '1510', correct: true}
+        ]    
+    },
+    {
+        question: 'How many different classes of Rum are there?',
+        answers: [
+            {text: '3', correct: false},
+            {text: '8', correct: true},
+            {text: '5', correct: false},
+            {text: '2', correct: false}
+        ]
+    },
+    {
+        question: 'Agave that claims to be 100% agave can still contain how much additive?',
+        answers: [
+            {text: '0%', correct: false},
+            {text: '1%', correct: true},
+            {text: '5%', correct: false},
+            {text: '10%', correct: false}
+        ]
+    },
+    {
+        question: 'Natural wine means?',
+        answers: [
+            {text: 'No/minimal intervention is made in the process ', correct: true},
+            {text: 'No yeast is added ', correct: false},
+            {text: 'Grapes are certified organic', correct: false},
+            {text: 'No suffer is added', correct: false}
+        ]
+    },
+    {
+        question: 'The 1751 depiction of Gin Ally shows:',
+        answers: [
+            {text: 'The joys that are, Gin', correct: false},
+            {text: 'The process of making Gin', correct: false},
+            {text: 'The taverns full of drunks', correct: false},
+            {text: 'The lewd behaviors of people consuming Gin ', correct: true}
+        ]
+    }
+]
+ 

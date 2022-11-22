5a1066372fde654b799bcbe67478708bd50f31f2
matt530bailey <115194527+matt530bailey@users.noreply.github.com>
2022-11-22 12:34:44 -0800

    created function to work through each question in the index along with answer key, fixed timer to stop at correct time, started building score keeper function

--- script.js
+++ script.js
@@ -3,88 +3,130 @@ var questionContainer = document.querySelector("#question_box")
 var timerEl = document.querySelector("#timer")
 var questionEl = document.querySelector(".question")
 var answerGrid = document.querySelector(".answers")
-var timeRemaining = 5
-var shuffledQuestions, currentQuestionIndex
+var score = document.querySelector('.score_card')
+var scoreKeeper = 0
+var questionIndex = 0
+var timeRemaining = 15
 
 start_quiz_btn.addEventListener('click', () => {
-    timer ();
+    timer();
     setNextQuestion();
     start_quiz_btn.classList.add("hide");
     questionContainer.classList.remove("hide");
     timerEl.classList.remove('hide')
-    
+
+
 
 })
 
 function timer() {
-    setInterval( () => {
+    setInterval(() => {
         timeRemaining--
         if (timeRemaining < 1) {
-            clearInterval(timerEl);
-            alert("Out of time!!");
             return;
-
         }
         console.log(timeRemaining);
-    }, 1000 );
+    }, 1000);
 }
 
+function checkGameOver() {
+    // if questionIndex < questions.length 
+    if (questionIndex < questions.length) {
+        true
+    } else {
+        score.innerHTML = scoreKeeper
+    }
+    // display score
+    // return false
+}
 
 function setNextQuestion() {
+    var currentQuesiton = questions[questionIndex];
+    var optionsContainer = document.querySelector('.options');
+    optionsContainer.innerHTML = ''
+    // find header element
+    console.log(document.querySelector('.question'));
+    // Get question 
+    console.log(questions[questionIndex].question);
+    // Set text cont for header
+    document.querySelector('.question').textContent = currentQuesiton.question
+    for (var i = 0; i < currentQuesiton.answers.length; i++) {
+        var choice = currentQuesiton.answers[i];
+        var button = document.createElement('button')
+        button.textContent = choice.text
+        button.addEventListener('click', selectAnswer)
+        button.setAttribute('data-is-correct', choice.correct)
+        optionsContainer.appendChild(button)
+
+    }
+
 }
 
-function showQuestion () {
-}
-
-function selectAnswer () {
+function showQuestion() {
+    // grabs first question & answer in array
 
 }
 
-let questions = [
+function selectAnswer(event) {
+    event.preventDefault()
+    console.log(event.target);
+    var button = event.target;
+    var isCorrect = Boolean(button.getAttribute('data-is-correct'));
+    // need to add -- to timer() for incorrect 
+    if (isCorrect) {
+        scoreKeeper++
+    } else {
+        timeRemaining -= 5
+    }
+    questionIndex++
+    setNextQuestion()
+}
+
+
+var questions = [
     {
         question: 'When does Benedictine claim to be invented?',
         answers: [
-            { text: '1775', correct: false},
-            { text: '1738', correct: false},
-            { text: '1313', correct: false},
-            { text: '1510', correct: true}
-        ]    
+            { text: '1775', correct: false },
+            { text: '1738', correct: false },
+            { text: '1313', correct: false },
+            { text: '1510', correct: true }
+        ]
     },
     {
         question: 'How many different classes of Rum are there?',
         answers: [
-            {text: '3', correct: false},
-            {text: '8', correct: true},
-            {text: '5', correct: false},
-            {text: '2', correct: false}
+            { text: '3', correct: false },
+            { text: '8', correct: true },
+            { text: '5', correct: false },
+            { text: '2', correct: false }
         ]
     },
     {
         question: 'Agave that claims to be 100% agave can still contain how much additive?',
         answers: [
-            {text: '0%', correct: false},
-            {text: '1%', correct: true},
-            {text: '5%', correct: false},
-            {text: '10%', correct: false}
+            { text: '0%', correct: false },
+            { text: '1%', correct: true },
+            { text: '5%', correct: false },
+            { text: '10%', correct: false }
         ]
     },
     {
         question: 'Natural wine means?',
         answers: [
-            {text: 'No/minimal intervention is made in the process ', correct: true},
-            {text: 'No yeast is added ', correct: false},
-            {text: 'Grapes are certified organic', correct: false},
-            {text: 'No suffer is added', correct: false}
+            { text: 'No/minimal intervention is made in the process ', correct: true },
+            { text: 'No yeast is added ', correct: false },
+            { text: 'Grapes are certified organic', correct: false },
+            { text: 'No suffer is added', correct: false }
         ]
     },
     {
         question: 'The 1751 depiction of Gin Ally shows:',
         answers: [
-            {text: 'The joys that are, Gin', correct: false},
-            {text: 'The process of making Gin', correct: false},
-            {text: 'The taverns full of drunks', correct: false},
-            {text: 'The lewd behaviors of people consuming Gin ', correct: true}
+            { text: 'The joys that are, Gin', correct: false },
+            { text: 'The process of making Gin', correct: false },
+            { text: 'The taverns full of drunks', correct: false },
+            { text: 'The lewd behaviors of people consuming Gin ', correct: true }
         ]
     }
 ]
- 
